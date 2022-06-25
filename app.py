822792eaafd6530df37f13364f17198a7f039cbd
Nchofon <ncho4real@gmail.com>
2022-06-25 17:20:42 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import adafruit_mlx90614
+import board
+from flask import Flask, jsonify, request
+# Importing modules for the board and mlx. Uncomment in production
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+# For body temperature
+@app.route('/body_temperature', methods=['GET'])
+def body_temperature():
+    i2c = board.I2C()
+    mlx = adafruit_mlx90614.MLX90614(i2c)
+    #return jsonify(massage="37.1")
+    return jsonify(massage=mlx.object_temperature)
+
+# For ambient temperature
+@app.route('/ambient_temperature', methods=['GET'])
+def ambient_temperature():
+    i2c = board.I2C()
+    mlx = adafruit_mlx90614.MLX90614(i2c)
+    #return jsonify(massage="37.1")
+    return jsonify(massage=mlx.ambient_temperature)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
